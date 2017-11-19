23adfaa2fb1eb6e29a3075b18fc9b4d63b8fd28c
pablich <pablich@MacBook-Pro-de-pablich.local>
2017-11-18 22:22:02 -0300

    Secure url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import Config from './config';
 
 const {apiKey} = Config;
-const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`;
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`;
 const URL_COUNTRIES = `https://restcountries.eu/rest/v2/all`;
 
 function getContries(){
